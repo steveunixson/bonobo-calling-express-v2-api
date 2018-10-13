7047bb83b64b5dad1d7a6120dda6325af0ab3336
Steve Unixson <unixson@gmail.com>
2018-10-13 16:29:51 +0300

    for loop single page only

--- controllers/grabber.js
+++ controllers/grabber.js
@@ -4,7 +4,7 @@ const baseURL = 'https://2gis.ru/rostov/';
 const search = 'Парикмахерская';
 const formSelector = 'div#module-1-3-1 > div > input';
 const submitButton = '#module-1-3 > div.searchBar__forms > div > form > button.searchBar__submit._directory';
-const width = 1980;
+const width = 1920;
 const height = 1050;
 
 const scrape = async () => {
@@ -23,28 +23,37 @@ const scrape = async () => {
   await page.click(submitButton);
   await console.log(`clicked on ${submitButton}`);
   await page.waitFor(5000);
-  const selectorID = await page.evaluate(() => {
-    const anchors = document.querySelectorAll('article');
-    const ids = [].map.call(anchors, article => article.id);
-    return ids;
-  });
-  return { selectorID, page };
+  const items = [];
+
+  function delay(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+  // document.getElementsByClassName('miniCard')[2]
+  // document.getElementsByClassName('miniCard')[3].attributes.id.nodeValue
+
+  for (let step = 0; step < 12; step++) {
+    const action = await page.evaluate(() => {
+      const anchors = document.getElementsByClassName('miniCard');
+      return [].map.call(anchors, a => a.id);
+    });
+    await page.click(`#${action[step]} > div`);
+    await console.log(`clicked on: #${action[step]} > div`);
+    await page.waitForSelector('div.contact__phonesFadeShow');
+    await page.click('div.contact__phonesFadeShow');
+    const number = await page.evaluate(() => document.querySelector('div.contact__phonesVisible').innerText);
+    items.push(number.replace(/(\r\n\t|\n|\r\t)/gm, ' ', ' ').split('Пожалуйста, скажите, что узнали номер в 2ГИС').join(''));
+    await delay(5000);
+  }
+  await page.focus('div.pagination__arrow._right');
+  await page.waitFor(500);
+  await page.click('div.pagination__arrow._right');
+  await console.log('clicked on: div.pagination__arrow._right');
+  return { selectorID: items };
 };
 scrape().then((value) => {
-  const promises = value.selectorID.forEach(async (selector, currentIndex) => {
-    await setTimeout(() => {
-      value.page.click(`#${selector} > div`);
-      value.page.waitFor(1000);
-      value.page.evaluate(() => {
-        const elements = document.querySelectorAll('a.cardActionsButton__btn._type_phone');
-        return [].map.call(elements, phoneSelector => phoneSelector.href);
-      }).then((phone) => {
-        console.log(phone);
-      });
-      console.log(`clicked on: #${selector} > div : ${currentIndex}`);
-    }, currentIndex * 2000);
-    await promises;
-  });// Получилось!
+  console.log(value);
 }).catch((exception) => {
   console.log(`EXCEPTION CAUGHT: ${exception}`);
 });
