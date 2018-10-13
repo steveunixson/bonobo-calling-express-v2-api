09a3a277f818dd65b4e6920be3ce62c0daa78c54
Steve Unixson <unixson@gmail.com>
2018-10-13 17:18:43 +0300

    added while loop and warped it in try/catch

--- controllers/grabber.js
+++ controllers/grabber.js
@@ -32,24 +32,33 @@ const scrape = async () => {
   }
   // document.getElementsByClassName('miniCard')[2]
   // document.getElementsByClassName('miniCard')[3].attributes.id.nodeValue
-
-  for (let step = 0; step < 12; step++) {
-    const action = await page.evaluate(() => {
-      const anchors = document.getElementsByClassName('miniCard');
-      return [].map.call(anchors, a => a.id);
-    });
-    await page.click(`#${action[step]} > div`);
-    await console.log(`clicked on: #${action[step]} > div`);
-    await page.waitForSelector('div.contact__phonesFadeShow');
-    await page.click('div.contact__phonesFadeShow');
-    const number = await page.evaluate(() => document.querySelector('div.contact__phonesVisible').innerText);
-    items.push(number.replace(/(\r\n\t|\n|\r\t)/gm, ' ', ' ').split('Пожалуйста, скажите, что узнали номер в 2ГИС').join(''));
-    await delay(5000);
+  await page.waitForSelector('div.pagination__arrow._right');
+  if (await page.$('div.pagination__arrow._right') === null || undefined) {
+    console.log('EXCEPTION CAUGHT: No visible element div.pagination__arrow._right');
   }
-  await page.focus('div.pagination__arrow._right');
-  await page.waitFor(500);
-  await page.click('div.pagination__arrow._right');
-  await console.log('clicked on: div.pagination__arrow._right');
+  try {
+    while (await page.$('div.pagination__arrow._right') !== null) {
+      for (let step = 0; step < 12; step++) {
+        const action = await page.evaluate(() => {
+          const anchors = document.getElementsByClassName('miniCard');
+          return [].map.call(anchors, a => a.id);
+        });
+        await page.click(`#${action[step]} > div`);
+        await console.log(`clicked on: #${action[step]} > div`);
+        await page.waitForSelector('div.contact__phonesFadeShow');
+        await page.click('div.contact__phonesFadeShow');
+        const number = await page.evaluate(() => document.querySelector('div.contact__phonesVisible').innerText);
+        items.push(number.replace(/(\r\n\t|\n|\r\t)/gm, ' ', ' ').split('Пожалуйста, скажите, что узнали номер в 2ГИС').join(''));
+        delay(5000);
+      }
+      await page.waitForSelector('div.pagination__arrow._right');
+      await page.focus('div.pagination__arrow._right');
+      await page.click('div.pagination__arrow._right');
+      await console.log('clicked on: div.pagination__arrow._right');
+    }
+  } catch (exception) {
+    console.log(`EXCEPTION CAUGHT: ${exception}`);
+  }
   return { selectorID: items };
 };
 scrape().then((value) => {
